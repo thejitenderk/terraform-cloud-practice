4a67d10e390e2f56a461d978f56227cf1eb7788c
jitender kumar <thejitenderk@gmail.com>
2025-12-25 11:31:44 +0530

    remove unused terraform block and provider configuration from main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.50.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {
-
-  }
-}
-
 resource "azurerm_resource_group" "rgdemo" {
   name     = "rg-demo-terraform-001"
   location = "East US"
