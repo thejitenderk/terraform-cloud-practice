9023625d1071f8661cbbacc179c93895bc12453b
jitender kumar <thejitenderk@gmail.com>
2025-12-25 11:30:29 +0530

    formatting improvements in main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,19 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "4.50.0"
+      source  = "hashicorp/azurerm"
+      version = "4.50.0"
     }
   }
 }
 
 provider "azurerm" {
   features {
-    
+
   }
+}
+
+resource "azurerm_resource_group" "rgdemo" {
+  name     = "rg-demo-terraform-001"
+  location = "East US"
 }
